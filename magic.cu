6d74eab0dfa050f76ad9c3649bdcc542b91fba4e
Ingafen <ingafen@gmail.com>
2022-06-14 17:00:07 +0500

    It works

--- magic.cu
+++ magic.cu
@@ -1,8 +1,123 @@
-extern "C" __global__
-void saxpy(float a, float *x, float *y, float *out, size_t n)
+#define C_IDX blockIdx.x * blockDim.x + threadIdx.x
+
+extern "C" __global__ void saxpy(float a, float *x, float *y, float *out, size_t n)
 {
- size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
- if (tid < n) {
-   out[tid] = a * x[tid] + y[tid];
- }
+  size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
+  if (tid < n) 
+  {
+    out[tid] = a * x[tid] + y[tid];
+  }
+}
+
+extern "C" __global__ void learn(float* x, float* w, float* l, float* y, float* sp, 
+  int* ida, int* bna, int* bca, int* s4, int* s3, int* row, int* sizes, 
+  float* yExp, float* phi, float* speed)
+{
+  
+  //Вычисление вектора y - скалярное произведение, вычисление путём редукции
+  ida[C_IDX] = blockIdx.x * blockDim.x + threadIdx.x;
+
+  while (ida[C_IDX] < sizes[0] * sizes[1]) //idx < k*M
+  {
+    sp[ida[C_IDX] % sizes[0] + (ida[C_IDX] / sizes[0]) * sizes[5]] = 
+      x[ida[C_IDX] % sizes[0]] * w[ida[C_IDX]];
+    
+    ida[C_IDX] += blockDim.x * gridDim.x;
+  }
+  
+  __syncthreads();
+   
+  ida[C_IDX] = blockIdx.x * blockDim.x + threadIdx.x;
+
+  __syncthreads();
+
+  while(s3[C_IDX] <= sizes[2]) //3-idx, 2-steps count
+  {    
+    __syncthreads();
+
+    for (row[C_IDX] = 0; row[C_IDX] < sizes[1]; row[C_IDX]++)
+    {
+      ida[C_IDX] = blockIdx.x * blockDim.x + threadIdx.x;
+
+      __syncthreads();
+
+      while (ida[C_IDX] < s4[C_IDX])  
+      {
+        bca[C_IDX] = (1 - s3[C_IDX] % 2 ) * sizes[5] * sizes[1];
+        bna[C_IDX] = (s3[C_IDX] % 2) * sizes[5] * sizes[1];
+
+        sp[row[C_IDX] * sizes[5] + ida[C_IDX] + bca[C_IDX]] = 
+          sp[row[C_IDX] * sizes[5] + 2 * ida[C_IDX] + 1 + bna[C_IDX]] 
+          + sp[row[C_IDX] * sizes[5] + 2 * ida[C_IDX] + bna[C_IDX]];
+
+        __syncthreads();   
+        sp[row[C_IDX] * sizes[5] + 2*ida[C_IDX] + 1 + bna[C_IDX]] = 0;
+        sp[row[C_IDX] * sizes[5] + 2*ida[C_IDX] + bna[C_IDX]] = 0;            
+       
+        __syncthreads();
+        ida[C_IDX] += blockDim.x * gridDim.x;
+
+        __syncthreads();
+      }
+    }
+	 
+    __syncthreads();
+	
+    s3[C_IDX]++;
+    s4[C_IDX]/=2;
+ 
+    __syncthreads();
+  }
+
+  ida[C_IDX] = blockIdx.x * blockDim.x + threadIdx.x;
+  
+  while(ida[C_IDX] < sizes[1])
+  {
+    y[ida[C_IDX]] = sp[ida[C_IDX] * sizes[5] + (s3[C_IDX] % 2) * sizes[5] * sizes[1]];
+    ida[C_IDX] += blockDim.x * gridDim.x;
+    
+    __syncthreads();
+  }
+
+  __syncthreads();
+
+  
+  //Вычисление exp(y) и sum(exp(y))
+  ida[C_IDX] = blockIdx.x * blockDim.x + threadIdx.x;
+  while(ida[C_IDX] < sizes[1])
+  {
+    yExp[ida[C_IDX]] = expf(y[ida[C_IDX]]);
+    atomicAdd(&yExp[sizes[1]], yExp[ida[C_IDX]]);
+    ida[C_IDX] += blockDim.x * gridDim.x;
+  }
+  __syncthreads();
+
+  //Вычисление phi(y)
+  ida[C_IDX] = blockIdx.x * blockDim.x + threadIdx.x;
+  while(ida[C_IDX] < sizes[1])
+  {
+    phi[ida[C_IDX]] = expf(y[ida[C_IDX]]) / yExp[sizes[1]];   
+    ida[C_IDX] += blockDim.x * gridDim.x;
+  }
+  __syncthreads();
+
+  
+  //корректировка весов
+  ida[C_IDX] = blockIdx.x * blockDim.x + threadIdx.x;
+
+  while ( ida[C_IDX] < sizes[0] * sizes[1] )
+  {
+    w[ida[C_IDX]] -= speed[0]
+      * x[ ida[C_IDX] % sizes[0] ] // x[i]
+      * ( 
+          ((1 - l[ida[C_IDX]/sizes[0]]) * phi[ida[C_IDX]/sizes[0]]) // i <> j | marker * phi_i 
+          +
+          (l[ida[C_IDX] / sizes[0]] * ( phi[ida[C_IDX]/sizes[0]] - 1 )) // i == j | marker * (phi_i-1)
+        );
+
+    ida[C_IDX] += blockDim.x * gridDim.x;
+  }
+
+  __syncthreads();
+  
 }
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from math import ceil, log2
+import cv2
 import numpy as np
 from cuda import cuda, nvrtc
 
@@ -11,99 +13,179 @@ def ASSERT_DRV(err):
     else:
         raise RuntimeError("Unknown error type: {}".format(err))
 
+def read_learn_sample(d):
+    img = cv2.imread('.\IR\l1.jpg', cv2.IMREAD_GRAYSCALE)
+    scale = 3
+    im_width = int(img.shape[1] / scale)
+    im_height = int(img.shape[0] / scale)
+    im_size = (im_width, im_height)
+    learnDataCount = 21
+    classes_count = 21
 
-cuda_code = ""
-with open("magic.cu") as f:
-    lines = f.readlines()
+    learn = np.zeros((learnDataCount, im_height * im_width), dtype=np.float32)
+    answer = np.zeros((learnDataCount, classes_count), dtype=np.float32)
 
-cuda_code = cuda_code.join(lines)
+    for i in range(0,learnDataCount):
+        tmp_img = cv2.imread(".\IR\l{}.jpg".format(str(i+1)), cv2.IMREAD_GRAYSCALE)
+        tmp_img = cv2.resize(tmp_img, im_size)
+        tmp = tmp_img.flatten()
+        learn[i,:] = tmp * d
+        answer[i,i] = 1.0
+    return learn, answer
+    
+def arr_size(x):
+    debugg = x.size * x.itemsize
+    return  debugg
 
-# Create program
-err, prog = nvrtc.nvrtcCreateProgram(str.encode(cuda_code), b"saxpy.cu", 0, [], [])
+def cuda_learn(x: np.array, l: np.array, w: np.array):
+    cuda_code = ""
+    with open("magic.cu") as f:
+        lines = f.readlines()
 
-# Compile program
-opts = []
-err, = nvrtc.nvrtcCompileProgram(prog, 0, opts)
+    cuda_code = cuda_code.join(lines)
+    err, prog = nvrtc.nvrtcCreateProgram(str.encode(cuda_code), b"saxpy.cu", 0, [], [])
+    opts = []
+    err, = nvrtc.nvrtcCompileProgram(prog, 0, opts)
+    err, ptxSize = nvrtc.nvrtcGetPTXSize(prog)
+    ptx = b" " * ptxSize
+    err, = nvrtc.nvrtcGetPTX(prog, ptx)
+    err, = cuda.cuInit(0)
+    err, cuDevice = cuda.cuDeviceGet(0)
+    err, context = cuda.cuCtxCreate(0, cuDevice)
+    ptx = np.char.array(ptx)
+    err, module = cuda.cuModuleLoadData(ptx.ctypes.data)
+    ASSERT_DRV(err)
+    err, kernel = cuda.cuModuleGetFunction(module, b"learn")
+    ASSERT_DRV(err)
 
-# Get PTX from compilation
-err, ptxSize = nvrtc.nvrtcGetPTXSize(prog)
-ptx = b" " * ptxSize
-err, = nvrtc.nvrtcGetPTX(prog, ptx)
-# Initialize CUDA Driver API
-err, = cuda.cuInit(0)
-# Retrieve handle for device 0
-err, cuDevice = cuda.cuDeviceGet(0)
-# Create context
-err, context = cuda.cuCtxCreate(0, cuDevice)
-# Load PTX as module data and retrieve function
-ptx = np.char.array(ptx)
-# Note: Incompatible --gpu-architecture would be detected here
-err, module = cuda.cuModuleLoadData(ptx.ctypes.data)
-ASSERT_DRV(err)
-err, kernel = cuda.cuModuleGetFunction(module, b"saxpy")
-ASSERT_DRV(err)
+    NUM_THREADS = 31  # Threads per block
+    NUM_BLOCKS = 1 # Blocks per grid
 
-NUM_THREADS = 10  # Threads per block
-NUM_BLOCKS = 10  # Blocks per grid
+    M = x.shape[0] # height * width
+    k = l.shape[0] # classes count
+    #w = np.zeros((k,M), dtype=np.float32) # neurons weight
+    y = np.zeros(k, dtype=np.float32) # inner product of weights and input vector
+    sp_hor_size = pow(2, ceil(log2(M)))
+    sp = np.zeros((2 * k * sp_hor_size), dtype=np.float32) # place for inner product and reduction
+    ida = np.zeros(NUM_BLOCKS * NUM_THREADS, dtype=np.int32) # place for cuda idx
+    bna = np.zeros(NUM_BLOCKS * NUM_THREADS, dtype=np.int32) # place for bias_nechet
+    bca = np.zeros(NUM_BLOCKS * NUM_THREADS, dtype=np.int32) # place for bias_chet
 
-a = np.array([2.0], dtype=np.float32)
-n = np.array(NUM_THREADS * NUM_BLOCKS, dtype=np.uint32)
-bufferSize = n * a.itemsize
+    sizes = np.zeros(12, dtype=np.int32) # place for indexes
+    sizes[0] = M
+    sizes[1] = k
+    sizes[2] = ceil(log2(M))-1 # reduction steps count
+    sizes[3] = 0 # reduction step idx
+    sizes[4] = sp_hor_size / 2 # reduction step limit
+    sizes[5] = sp_hor_size   
+    s4 = np.full(NUM_BLOCKS * NUM_THREADS, sizes[4], dtype=np.int32) #sizes[2]
+    s3 = np.full(NUM_BLOCKS * NUM_THREADS, sizes[3], dtype=np.int32) #sizes[3]
+    row = np.zeros(NUM_BLOCKS * NUM_THREADS, dtype=np.int32) #memory for for loop
+    yExp = np.zeros(sizes[1] + 1, dtype=np.float32) #memory for exp(y), additional element - for sum(exp(y))
+    phi = np.zeros(sizes[1], dtype=np.float32) # for phy(y)
+    speed = np.full(1, 0.5, dtype=np.float32) # learning speed rate
 
-hX = np.random.rand(n).astype(dtype=np.float32)
-hY = np.random.rand(n).astype(dtype=np.float32)
-hOut = np.zeros(n).astype(dtype=np.float32)
+    err, stream = cuda.cuStreamCreate(0)
 
-err, dXclass = cuda.cuMemAlloc(bufferSize)
-err, dYclass = cuda.cuMemAlloc(bufferSize)
-err, dOutclass = cuda.cuMemAlloc(bufferSize)
+    err, d_x_class = cuda.cuMemAlloc(arr_size(x))
+    err, = cuda.cuMemcpyHtoDAsync(d_x_class, x.ctypes.data, arr_size(x), stream)
+    err, d_l_class = cuda.cuMemAlloc(arr_size(l))
+    err, = cuda.cuMemcpyHtoDAsync(d_l_class, l.ctypes.data, arr_size(l), stream)
+    err, d_w_class = cuda.cuMemAlloc(arr_size(w))
+    err, = cuda.cuMemcpyHtoDAsync(d_w_class, w.ctypes.data, arr_size(w), stream)
+    err, d_y_class = cuda.cuMemAlloc(arr_size(y))
+    err, = cuda.cuMemcpyHtoDAsync(d_y_class, y.ctypes.data, arr_size(y), stream)
+    err, d_sp_class = cuda.cuMemAlloc(arr_size(sp))
+    err, = cuda.cuMemcpyHtoDAsync(d_sp_class, sp.ctypes.data, arr_size(sp), stream)
+    err, d_ida_class = cuda.cuMemAlloc(arr_size(ida))
+    err, = cuda.cuMemcpyHtoDAsync(d_ida_class, ida.ctypes.data, arr_size(ida), stream)
+    err, d_bna_class = cuda.cuMemAlloc(arr_size(bna))
+    err, = cuda.cuMemcpyHtoDAsync(d_bna_class, bna.ctypes.data, arr_size(bna), stream)
+    err, d_bca_class = cuda.cuMemAlloc(arr_size(bca))
+    err, = cuda.cuMemcpyHtoDAsync(d_bca_class, bca.ctypes.data, arr_size(bca), stream)
+    err, d_s4_class = cuda.cuMemAlloc(arr_size(s4))
+    err, = cuda.cuMemcpyHtoDAsync(d_s4_class, s4.ctypes.data, arr_size(s4), stream)
+    err, d_s3_class = cuda.cuMemAlloc(arr_size(s3))
+    err, = cuda.cuMemcpyHtoDAsync(d_s3_class, s3.ctypes.data, arr_size(s3), stream)
+    err, d_row_class = cuda.cuMemAlloc(arr_size(row))
+    err, = cuda.cuMemcpyHtoDAsync(d_row_class, row.ctypes.data, arr_size(row), stream)
+    err, d_sizes_class = cuda.cuMemAlloc(arr_size(sizes))
+    err, = cuda.cuMemcpyHtoDAsync(d_sizes_class, sizes.ctypes.data, arr_size(sizes), stream)
+    err, d_yExp_class = cuda.cuMemAlloc(arr_size(yExp))
+    err, = cuda.cuMemcpyHtoDAsync(d_yExp_class, yExp.ctypes.data, arr_size(yExp), stream)
+    err, d_phi_class = cuda.cuMemAlloc(arr_size(phi))
+    err, = cuda.cuMemcpyHtoDAsync(d_phi_class, phi.ctypes.data, arr_size(phi), stream)
+    err, d_speed_class = cuda.cuMemAlloc(arr_size(speed))
+    err, = cuda.cuMemcpyHtoDAsync(d_speed_class, speed.ctypes.data, arr_size(speed), stream)
 
-err, stream = cuda.cuStreamCreate(0)
+    d_x = np.array([int(d_x_class)], dtype=np.uint64)
+    d_l = np.array([int(d_l_class)], dtype=np.uint64)
+    d_w = np.array([int(d_w_class)], dtype=np.uint64)
+    d_y = np.array([int(d_y_class)], dtype=np.uint64)
+    d_sp = np.array([int(d_sp_class)], dtype=np.uint64)
+    d_ida = np.array([int(d_ida_class)], dtype=np.uint64)
+    d_bna = np.array([int(d_bna_class)], dtype=np.uint64)
+    d_bca = np.array([int(d_bca_class)], dtype=np.uint64)
+    d_s4 = np.array([int(d_s4_class)], dtype=np.uint64)
+    d_s3 = np.array([int(d_s3_class)], dtype=np.uint64)
+    d_row = np.array([int(d_row_class)], dtype=np.uint64)
+    d_sizes = np.array([int(d_sizes_class)], dtype=np.uint64)
+    d_yExp = np.array([int(d_yExp_class)], dtype=np.uint64)
+    d_phi = np.array([int(d_phi_class)], dtype=np.uint64)
+    d_speed = np.array([int(d_speed_class)], dtype=np.uint64)
 
-err, = cuda.cuMemcpyHtoDAsync(
-   dXclass, hX.ctypes.data, bufferSize, stream
-)
-err, = cuda.cuMemcpyHtoDAsync(
-   dYclass, hY.ctypes.data, bufferSize, stream
-)
+    args = [d_x, d_w, d_l, d_y, d_sp, d_ida, d_bna, d_bca, d_s4, d_s3, d_row, d_sizes, d_yExp, d_phi, d_speed]    
+    args = np.array([arg.ctypes.data for arg in args], dtype=np.uint64)
 
-# The following code example is not intuitive 
-# Subject to change in a future release
-dX = np.array([int(dXclass)], dtype=np.uint64)
-dY = np.array([int(dYclass)], dtype=np.uint64)
-dOut = np.array([int(dOutclass)], dtype=np.uint64)
+    err, = cuda.cuLaunchKernel(
+    kernel,
+    NUM_BLOCKS,  # grid x dim
+    1,  # grid y dim
+    1,  # grid z dim
+    NUM_THREADS,  # block x dim
+    1,  # block y dim
+    1,  # block z dim
+    0,  # dynamic shared memory
+    stream,  # stream
+    args.ctypes.data,  # kernel arguments
+    0,  # extra (ignore)
+    )
 
-args = [a, dX, dY, dOut, n]
-args = np.array([arg.ctypes.data for arg in args], dtype=np.uint64)
+    err, = cuda.cuMemcpyDtoHAsync(w.ctypes.data, d_w_class, arr_size(w), stream)
+    err, = cuda.cuMemcpyDtoHAsync(phi.ctypes.data, d_phi_class, arr_size(phi), stream)
+    err, = cuda.cuStreamSynchronize(stream)
 
-err, = cuda.cuLaunchKernel(
-   kernel,
-   NUM_BLOCKS,  # grid x dim
-   1,  # grid y dim
-   1,  # grid z dim
-   NUM_THREADS,  # block x dim
-   1,  # block y dim
-   1,  # block z dim
-   0,  # dynamic shared memory
-   stream,  # stream
-   args.ctypes.data,  # kernel arguments
-   0,  # extra (ignore)
-)
+    print(phi)
+    print("")
+    
+    err, = cuda.cuStreamDestroy(stream)
+    err, = cuda.cuMemFree(d_x_class)
+    err, = cuda.cuMemFree(d_w_class)
+    err, = cuda.cuMemFree(d_l_class)
+    err, = cuda.cuMemFree(d_y_class)
+    err, = cuda.cuMemFree(d_sp_class)
+    err, = cuda.cuMemFree(d_ida_class)
+    err, = cuda.cuMemFree(d_bna_class)
+    err, = cuda.cuMemFree(d_bca_class)
+    err, = cuda.cuMemFree(d_s3_class)
+    err, = cuda.cuMemFree(d_s4_class)
+    err, = cuda.cuMemFree(d_sizes_class)
+    err, = cuda.cuMemFree(d_row_class)
+    err, = cuda.cuMemFree(d_sizes) 
+    err, = cuda.cuMemFree(d_yExp) 
+    err, = cuda.cuMemFree(d_phi) 
+    err, = cuda.cuMemFree(d_speed) 
+    err, = cuda.cuModuleUnload(module)
+    err, = cuda.cuCtxDestroy(context)
+    return w
 
-err, = cuda.cuMemcpyDtoHAsync(
-   hOut.ctypes.data, dOutclass, bufferSize, stream
-)
-err, = cuda.cuStreamSynchronize(stream)
-# Assert values are same after running kernel
-hZ = a * hX + hY
-if not np.allclose(hOut, hZ):
-   raise ValueError("Error outside tolerance for host-device vectors")
-else: 
-    print("Сработало!")
 
-err, = cuda.cuStreamDestroy(stream)
-err, = cuda.cuMemFree(dXclass)
-err, = cuda.cuMemFree(dYclass)
-err, = cuda.cuMemFree(dOutclass)
-err, = cuda.cuModuleUnload(module)
-err, = cuda.cuCtxDestroy(context)
+ll = read_learn_sample(1e-4)
+M = ll[0].shape[1] # height * width
+k = ll[1].shape[0] # classes count
+w1 = np.zeros((k * M), dtype=np.float32) # neurons weight
+
+for n in range(0,4):
+    for i in range(0, ll[0].shape[0]):
+        print("Обучение образцом "+str(i) + " проход " + str(n))
+        w1 = cuda_learn(x=ll[0][i], l=ll[1][i], w=w1)
